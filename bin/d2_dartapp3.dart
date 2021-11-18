f68769177c0b7ba0941860c79ab89f0b2daacad6
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-18 09:20:08 +0700

    initial commit

---
+++ bin/d2_dartapp3.dart
@@ -0,0 +1,5 @@
+import 'package:d2_dartapp3/d2_dartapp3.dart' as d2_dartapp3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${d2_dartapp3.calculate()}!');
+}
---
+++ test/d2_dartapp3_test.dart
@@ -0,0 +1,8 @@
+import 'package:d2_dartapp3/d2_dartapp3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
